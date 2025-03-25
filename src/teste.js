acc201977a3b6e6c502315876a2e60ca291c70fd
IanAlmeida247 <almeidaian318@gmail.com>
2025-03-25 15:05:35 -0300

    feat: subindo arquivos

---
+++ src/teste.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplo de uso:
+console.log(multiplicar(2, 3)); // Saída: 6
+console.log(saudacao("João")); // Saída: Olá João
---
+++ src/teste.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplo de uso:
+console.log(multiplicar(2, 3));  // Saída: 6
+console.log(saudacao("João"));   // Saída: Olá João
